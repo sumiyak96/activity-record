5d82045f2b679b11be82718a67592ab9e05f7b84
sumiyaaax <wave_soul14@yahoo.co.jp>
2024-02-23 15:54:48 +0900

    first commit

---
+++ src/main/kotlin/com/activity/activityrecord/ActivityRecordApplication.kt
@@ -0,0 +1,11 @@
+package com.activity.activityrecord
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActivityRecordApplication
+
+fun main(args: Array<String>) {
+	runApplication<ActivityRecordApplication>(*args)
+}
---
+++ src/test/kotlin/com/activity/activityrecord/ActivityRecordApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.activity.activityrecord
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActivityRecordApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
